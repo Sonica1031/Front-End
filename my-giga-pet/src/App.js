988985bc17bc74086241f9f2855f96ccd3efa7e9
Sonica1031 <Sonica1031@hotmail.com>
2020-01-29 07:13:21 -0800

    Animation at different speeds

--- my-giga-pet/src/App.js
+++ my-giga-pet/src/App.js
@@ -8,17 +8,19 @@ import Broccoli from './img/Broccoli.png';
 import './App.css';
 
 function App() {
-  const SlideInDown = styled.div`animation 20s ${keyframes `${slideInDown}`} infinite`
+  const SlideInDown = styled.div`animation 10s ${keyframes `${slideInDown}`} infinite`
+  const SlideDown= styled.div`animation 20s ${keyframes `${slideInDown}`} infinite`
   return (
     <div className="App">
       <header className="App-header">
         <div className="App-logo">
           <img src={logo} alt="logo" width="300px" />
-          <div className="Sliders">
-          <SlideInDown><img src={Carrot} alt="falling-carrot" /></SlideInDown>
-          <SlideInDown><img src={Lettuce} alt="falling-lettuce" /></SlideInDown>
-          <SlideInDown><img src={Broccoli} alt="falling-broccoli" /></SlideInDown>
           </div>
+          <div className="Sliders">
+          <SlideInDown><img src={Carrot} alt="falling-carrot" width="250px" /></SlideInDown>
+          <SlideDown><img src={Broccoli} alt="falling-broccoli" /></SlideDown>
+          <SlideInDown><img src={Carrot} alt="falling-carrot" width="250px" /></SlideInDown>
+          <SlideDown><img src={Lettuce} alt="falling-lettuce" /></SlideDown>
         </div>
         <h1>Giga Pet</h1>
         <p>The fun way to eat healthy!</p>
